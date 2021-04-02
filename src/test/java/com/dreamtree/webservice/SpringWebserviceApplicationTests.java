2b32ac7ac03868094569a13cb5202590af2f2b6d
zeekgwang@gmail.com <rhkd1206!>
2021-04-02 20:10:36 +0900

    initial commit

---
+++ src/test/java/com/dreamtree/webservice/SpringWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamtree.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
