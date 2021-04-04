3fdb77920b0eb76e66108acc7eafdcd1c162d54f
zeekgwang@gmail.com <rhkd1206!>
2021-04-04 15:52:22 +0900

    2강 완료

--- src/main/java/com/dreamtree/webservice/web/WebRestController.java
+++ src/main/java/com/dreamtree/webservice/web/WebRestController.java
@@ -1,13 +1,26 @@
 package com.dreamtree.webservice.web;
 
+import com.dreamtree.webservice.domain.posts.PostsRepository;
+import com.dreamtree.webservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
     }
 }
