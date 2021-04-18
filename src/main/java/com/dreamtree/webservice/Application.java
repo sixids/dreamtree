3c845a264ad228d3023168d1d9d768556a1b5f0b
zeekgwang@gmail.com <rhkd1206!>
2021-04-18 23:54:01 +0900

    profile

--- src/main/java/com/dreamtree/webservice/Application.java
+++ src/main/java/com/dreamtree/webservice/Application.java
@@ -2,14 +2,22 @@ package com.dreamtree.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/dreamtree-webservice/real-application.yml";
+//            + "C:\\app\\config\\dreamtree-webservice\\real-application.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
 }
